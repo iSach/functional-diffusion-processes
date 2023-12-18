ba8a13bf2b82e14ac8e4773529bc93129a016580
Sacha Lewin <sacha.lewin@me.com>
2023-12-18 10:56:52 +0100

    removed wrong import

--- src/functional_diffusion_processes/sdetools/__init__.py
+++ src/functional_diffusion_processes/sdetools/__init__.py
@@ -1,5 +1,5 @@
 from .base_sde import SDE
 from .heat_subvp_sde import HeatSubVPSDE
-from .vp_sde import VPSDE
+#from .vp_sde import VPSDE
 
-__all__ = ["SDE", "HeatSubVPSDE", "VPSDE"]
+__all__ = ["SDE", "HeatSubVPSDE"]#, "VPSDE"]
